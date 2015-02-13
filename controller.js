2d2827797f867df6bba8cabe7d446467a13a17fd
Jesse Mace <jesmac87@gmail.com>
2015-02-12 19:42:39 -0700

    finish mini-userProfiles2

---
+++ controller.js
@@ -0,0 +1,14 @@
+var app = angular.module('userProfiles');
+
+app.controller('MainController', function($scope, mainService) {
+
+    $scope.getUsers = function() {
+        mainService.getUsers().then(function(users) {
+            $scope.users = users;
+        });
+    };
+
+
+    $scope.getUsers();
+
+});
---
+++ service.js
@@ -0,0 +1,22 @@
+var app = angular.module('userProfiles'); //wow
+
+app.service('mainService', function($http, $q) {
+
+
+    this.getUsers = function() {
+        var deferred = $q.defer();
+
+        $http({
+            method: 'GET',
+            url: 'http://reqr.es/api/users?page=1'
+        }).then(function(response) {
+            deferred.resolve(response.data.data);
+        }, function(err) {
+            deferred.reject(err);
+        });
+
+        return deferred.promise;
+    };
+
+
+});
